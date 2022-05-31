d2ba8820a2d50a0a83754b0ba29c44cc86b96452
Webdev1011 <92213116+Webdev1011@users.noreply.github.com>
2022-05-31 18:39:52 +0530

    DB config changed and tested

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const dbConn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "music-app",
+  host: "db4free.net",
+  user: "bat6vwpo",
+  password: "Ycxp@JG1agpi",
+  database: "songsapp",
 });
 
 dbConn.connect(function (err) {
